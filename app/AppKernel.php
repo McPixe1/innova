0ac1de9c92dc1d8be531909f88cbc32d53f7aaac
Samuel Torres <samuel.torres@e-campus.uab.es>
2017-05-13 18:18:28 +0200

    añadido y configurado FOSUserBundle y SonataAdmin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,18 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            
+            // FOSUserBundle
+            new FOS\UserBundle\FOSUserBundle(),
+            
+            // Bundles necesarios para Sonata
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+
+            // SonataAdminBundle
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
