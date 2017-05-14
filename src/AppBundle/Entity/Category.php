9136aad5133ef6635e9427a34cea56ae3aa35aa6
Samuel Torres <samuel.torres@e-campus.uab.es>
2017-05-14 21:02:28 +0200

    creadas entidades product y category creando layout basico

---
+++ src/AppBundle/Entity/Category.php
@@ -0,0 +1,41 @@
+<?php
+
+// src/AppBundle/Entity/Category.php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="category")
+ */
+class Category {
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /*
+     * @ORM\OneToMany(targetEntity="Product", mappedBy="category")
+     */
+    protected $products;
+
+    public function __construct() {
+        $this->products = new ArrayCollection();
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
