c16b80b0040662179571e1f501e1b038c75e6cf8
Dev <dev@lvp.es>
2017-05-16 17:54:27 +0200

    listado productos

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,17 +2,32 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
+use AppBundle\Entity\Product;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * 
      */
     public function indexAction(Request $request)
     {
         return $this->render('home/index.html.twig');
+    }
+    
+    
+    /**
+     * @Route("/products", name="products")
+     * 
+     */
+    public function productShowAction(){
+        $products = $this->getDoctrine()->getRepository(Product::class)->findAll();
+        
+        return $this->render('home/products.html.twig', ['products' => $products]);
     }
 }
