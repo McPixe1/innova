22bf4d7c391cfd288d1fc6fa711cb374199dc0f4
Dev <dev@lvp.es>
2017-05-17 17:13:13 +0200

    -

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,7 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Product;
 
-
 class DefaultController extends Controller {
 
     /**
@@ -35,53 +34,45 @@ class DefaultController extends Controller {
      */
     public function contactAction(Request $request) {
 
-        // Create the form according to the FormType created previously.
-        // And give the proper parameters
+
         $form = $this->createForm('AppBundle\Form\ContactType', null, array(
-            // To set the action use $this->generateUrl('route_identifier')
             'action' => $this->generateUrl('contact'),
             'method' => 'POST'
         ));
 
         if ($request->isMethod('POST')) {
-            // Refill the fields in case the form is not valid.
             $form->handleRequest($request);
 
             if ($form->isValid()) {
-                // Send mail
                 if ($this->sendEmail($form->getData())) {
-
                     return $this->redirectToRoute('homepage');
                 } else {
-                    // An error ocurred, handle
-                    var_dump("Errooooor :(");
+                    var_dump("Error");
                 }
             }
         }
-
         return $this->render('innova/contact.html.twig', array(
                     'form' => $form->createView()
         ));
     }
-    
-     private function sendEmail($data){
+
+    private function sendEmail($data) {
         $myappContactMail = 'torres.88.bcn@gmail.com';
         $myappContactPassword = '-'; //PONER AQUI EL PWD CORRESPONDIENTE
-        
         // http://ourcodeworld.com/articles/read/14/swiftmailer-send-mails-from-php-easily-and-effortlessly
-        $transport = \Swift_SmtpTransport::newInstance('smtp.gmail.com', 465,'ssl')
-            ->setUsername($myappContactMail)
-            ->setPassword($myappContactPassword);
+        $transport = \Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
+                ->setUsername($myappContactMail)
+                ->setPassword($myappContactPassword);
 
         $mailer = \Swift_Mailer::newInstance($transport);
-        
+
         $message = \Swift_Message::newInstance($data["subject"])
-        ->setFrom(array($myappContactMail => "Mensaje de ".$data["name"]))
-        ->setTo(array(
-            $myappContactMail => $myappContactMail
-        ))
-        ->setBody($data["message"]."<br>ContactMail :".$data["email"]);
-        
+                ->setFrom(array($myappContactMail => "Mensaje de " . $data["name"]))
+                ->setTo(array(
+                    $myappContactMail => $myappContactMail
+                ))
+                ->setBody($data["message"] . "<br>Contacto :" . $data["email"]);
+
         return $mailer->send($message);
     }
 
