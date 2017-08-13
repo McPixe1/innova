6a348b86c3bd86b88f355d837cd27cf7825f86e4
Samuel Torres <samuel.torres@e-campus.uab.es>
2017-08-13 17:31:49 +0200

    terminada funcionalidad y estilo breadcrumbs

--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -2,7 +2,11 @@
 
 namespace AppBundle\Menu;
 
+use AppBundle\Entity\Product;
 use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class MenuBuilder {
@@ -12,46 +16,45 @@ class MenuBuilder {
     /**
      * @param FactoryInterface $factory
      */
-    public function __construct(FactoryInterface $factory) {
+    public function __construct(FactoryInterface $factory, ContainerInterface $container) {
         $this->factory = $factory;
+        $this->container = $container;
     }
 
     public function createMainMenu(RequestStack $requestStack) {
         $menu = $this->factory->createItem('root');
 
-        $menu->addChild('Home', array('route' => 'homepage'));
+        $menu->addChild('Inicio', array('route' => 'homepage'));
+
         $route = $requestStack->getCurrentRequest()->get('_route');
-        dump($route);
+
         switch ($route) {
-            case 'products':
-                $menu
-                        ->addChild('Productos')
-                        ->setCurrent(true)
-                ;
+
+            case ('products'):
+                $menu->addChild('Productos')->setCurrent(true);
                 break;
-            case 'products_filtered':
-                $menu
-                        ->addChild('Productos')
-                        ->setCurrent(true)
-                ;
+            case ('products_filtered'):
+                $menu->addChild('Productos')->setCurrent(true);
                 break;
-             case 'product':
-                $menu
-                        ->addChild('Productos')
-                        ->setCurrent(true)
-                ;
+
+            case 'product':
+                $productId = $requestStack->getCurrentRequest()->get('id');
+
+                $em = $this->container->get('doctrine.orm.entity_manager');
+
+                $product = $em->getRepository(Product::class)->findOneBy(array('id' => $productId));
+
+                $productName = $product->getName();
+                $menu->addChild('Productos', array('route' => 'products'));
+                $menu->addChild($productName);
                 break;
+
             case 'gallery':
-                $menu
-                        ->addChild('Galería')
-                        ->setCurrent(true)
-                ;
+                $menu->addChild('Galería')->setCurrent(true);
                 break;
+
             case 'contact':
-                $menu
-                        ->addChild('Contacto')
-                        ->setCurrent(true)
-                ;
+                $menu->addChild('Contacto')->setCurrent(true);
                 break;
         }
 
